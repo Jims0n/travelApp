f081b103d741f21cee6538314a07906d2ff757db
Jimson <opeyemiabdulateef406@gmail.com>
2023-12-31 23:39:35 +0100

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
 import './globals.css'
+import Navbar from '@/components/Navbar'
+import Footer from '@/components/Footer'
 
-const inter = Inter({ subsets: ['latin'] })
+
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -16,7 +17,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>
+        <Navbar />
+          <main className='relative overflow-hidden'>
+          {children}
+          </main>
+        
+        <Footer />
+        </body>
     </html>
   )
 }
